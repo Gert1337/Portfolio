7f2e7ec35b0d5072d7ffcfb76d6423440e709596
Gert1337 <Katharinaappel@gmail.com>
2025-04-19 14:14:39 +0200

    updated argtypes and add children as args

--- portfolio/src/app/components/Button/Button.stories.tsx
+++ portfolio/src/app/components/Button/Button.stories.tsx
@@ -2,9 +2,21 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import {Button} from './Button';
 
-const meta = {
-  component: Button,
-} satisfies Meta<typeof Button>;
+const meta: Meta<typeof Button> = {
+	component: Button,
+	args: {
+		children: 'Click me', // 👈 Default text shown for the button
+	},
+  tags: ['autodocs'],
+	argTypes: {
+    variant: {
+      control: "select"
+    },
+		children: {
+			control: 'text', // 👈 Allows editing in the Storybook controls panel
+		},
+	},
+};
 
 export default meta;
 
