5e1b4f922accd9304791e3ae4f5e4966b61bd88b
Gert1337 <Katharinaappel@gmail.com>
2025-04-19 14:25:42 +0200

    button sizing

--- portfolio/src/app/components/Button/Button.stories.tsx
+++ portfolio/src/app/components/Button/Button.stories.tsx
@@ -6,12 +6,17 @@ const meta: Meta<typeof Button> = {
 	component: Button,
 	args: {
 		children: 'Click me', // 👈 Default text shown for the button
+    variant: 'primary',
+    size: 'md',
 	},
   tags: ['autodocs'],
 	argTypes: {
     variant: {
       control: "select"
     },
+    size: {
+			control: "select",
+		},
 		children: {
 			control: 'text', // 👈 Allows editing in the Storybook controls panel
 		},
@@ -22,7 +27,30 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const PrimarySmall: Story = {
+  args: {
+    children: "Click me",
+    variant: "primary",
+    size: "sm"
+  }
+};
+export const PrimaryMedium: Story = {  
+  args: {
+  children: "Click me",
+  variant: "primary",
+  size: "md"
+}};
+
+
+
+export const PrimaryLarge: Story = {
+  args: {
+    children: "Click me",
+    variant: "primary",
+    size: "lg"
+  }
+};
+
 export const Outlined: Story = {args:{variant:'outlined'}};
 
 export const Critical: Story = {
--- portfolio/src/app/components/Button/Button.tsx
+++ portfolio/src/app/components/Button/Button.tsx
@@ -2,11 +2,13 @@ import clsx from 'clsx';
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
 	variant?: 'primary' | 'outlined' | 'critical';
+    size?: 'sm' | 'md' | 'lg';
 };
 
 export const Button = ({
 	children,
 	variant = 'primary',
+    size = 'md',
 	className,
 	...props
 }: ButtonProps) => {
@@ -18,6 +20,11 @@ export const Button = ({
 					'bg-blue-600 text-white hover:bg-blue-700': variant === 'primary',
 					'border border-blue-600 text-blue-600 hover:bg-blue-50': variant === 'outlined',
 					'bg-red-600 text-white hover:bg-red-700': variant === 'critical',
+				},
+                {
+					'px-3 py-1 text-sm': size === 'sm',
+					'px-4 py-2 text-base': size === 'md',
+					'px-6 py-3 text-lg': size === 'lg',
 				},
 				className
 			)}
